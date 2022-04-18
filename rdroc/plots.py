854503f38547a92fb10c726039798d15f4444f58
janaisawto <jrganais@gmail.com>
2022-04-17 20:04:28 -0400

    Added lasso and rectangular selection tools to the plots

--- rdroc/plots.py
+++ rdroc/plots.py
@@ -1,4 +1,5 @@
 from dash import Dash, dcc, html, Input, Output
+import numpy as np
 import plotly.express as px
 
 from rdroc.models import StarCluster
@@ -35,38 +36,54 @@ def dash_plot(input_dict: dict[str, StarCluster]) -> Dash:
         ]
     )
 
+    def get_figure(df, x_col, y_col, selectedpoints, selectedpoints_local):
+        """Get figure based on selected points. Extracted from https://dash.plotly.com/interactive-graphing"""
+
+        fig = px.scatter(df, x=df[x_col], y=df[y_col])
+        fig.update_traces(
+            selectedpoints=selectedpoints,
+            customdata=df.index,
+            marker={"opacity": 0.9},
+            unselected={
+                "marker": {"opacity": 0.1},
+            },
+        )
+
+        return fig
+
     @app.callback(
         Output("spatial-graphic", "figure"),
-        Input("cluster-selection-dropdown", "value"),
-    )
-    def update_figure(selected_cluster: str):
-        cluster = input_dict[selected_cluster]
-        df = cluster.datatable.to_pandas()
-        fig = px.scatter(df, x="RA_ICRS", y="DE_ICRS", hover_name="Gaia")
-        fig.update_layout(transition_duration=100)
-        return fig
-
-    @app.callback(
         Output("pm-graphic", "figure"),
-        Input("cluster-selection-dropdown", "value")
-    )
-    def update_figure_pm(selected_cluster: str):
-        cluster = input_dict[selected_cluster]
-        df = cluster.datatable.to_pandas()
-        fig = px.scatter(df, x="pmRA_", y="pmDE")
-        fig.update_layout(transition_duration=100)
-        return fig
-
-    @app.callback(
         Output("cmd-graphic", "figure"),
-        Input("cluster-selection-dropdown", "value")
+        Input("cluster-selection-dropdown", "value"),
+        Input("spatial-graphic", "selectedData"),
+        Input("pm-graphic", "selectedData"),
+        Input("cmd-graphic", "selectedData"),
     )
-    def update_figure_cmd(selected_cluster: str):
+    def update_figure(
+        selected_cluster, spatial_selected_data, pm_selected_data, cmd_selected_data
+    ):
         cluster = input_dict[selected_cluster]
         df = cluster.datatable.to_pandas()
-        fig = px.scatter(df, x="BP-RP", y="Gmag")
-        fig["layout"]["yaxis"]["autorange"] = "reversed"
-        fig.update_layout(transition_duration=100)
-        return fig
+        selectedpoints = df.index
+        for selected_data in [
+            spatial_selected_data,
+            pm_selected_data,
+            cmd_selected_data,
+        ]:
+            if selected_data and selected_data["points"]:
+                selectedpoints = np.intersect1d(
+                    selectedpoints, [p["customdata"] for p in selected_data["points"]]
+                )
+        
+        # Reverse axis for CMD plot
+        fig3 = get_figure(df, "BP-RP", "Gmag", selectedpoints, spatial_selected_data)
+        fig3["layout"]["yaxis"]["autorange"] = "reversed"
+        
+        return [
+            get_figure(df, "RA_ICRS", "DE_ICRS", selectedpoints, spatial_selected_data),
+            get_figure(df, "pmRA_", "pmDE", selectedpoints, spatial_selected_data),
+            fig3,
+        ]
 
     return app
